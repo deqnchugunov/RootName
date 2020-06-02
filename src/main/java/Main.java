336d513f5aaaf34c788da9ea8376a6c2d741f724
Deyan Chugunov <deyan.chugunov@aptsys.net>
2020-06-02 14:30:50 +0300

    main.java changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import RootNameExample.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Person person = new Person("John", 30, "Wall Street");
+        Person person = new Person("John", 35, "Wall Street");
         JsonMapper json = new JsonMapper(person);
         System.out.println(json.toString());
     }
